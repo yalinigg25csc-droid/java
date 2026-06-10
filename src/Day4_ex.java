e2c397cb20c6128324acb665dbd372edc58e34d5
yalinigg25csc-droid <yalinigg25csc@vetias.ac.in>
2026-06-10 14:55:03 +0530

    first commit

---
+++ src/Day4_ex.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Day4_ex {
+   /* void main() {
+
+        int[][] marks = new int[5][3];
+        int[][] mark = {
+                {78, 90, 34},
+                {89, 65, 89},
+                {12, 34, 56},
+                {66, 12, 89},
+                {33, 77, 88},
+        };
+        System.out.println("S.no\tName\tM\tP\tC\tTotal\tAverage");
+        for (int row = 0; row < marks.length; row++) {
+            int total = 0;
+            for (int col = 0; col < marks[row].length; col++) {
+                total += mark[row][col];
+
+            }
+            double average = total / 3.0;
+            System.out.printf("%d\tstudent%d\t%d\t%d\t%d\t%d\t%.2f\n",
+                     row+1,
+                    row+1,
+                    mark[row][0],
+                    mark[row][1],
+                    mark[row][2],
+                    total,
+                    average);
+        }
+    }
+
+}*\
+
+    */
+
---
+++ src/Day5.java
@@ -0,0 +1,16 @@
+gitimport java.util.Scanner;
+
+
+    void main(){
+        System.out.println("Enter your name:");
+        Scanner sc = new Scanner(System.in);
+        String name= sc.nextLine();
+        System.out.println("Enter your dept:");
+        String dept= sc.nextLine();
+        System.out.println("Enter your age:");
+        String age= sc.nextLine();
+
+
+
+    }
+
