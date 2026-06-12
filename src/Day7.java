a1594db49bedd7263b7659f2a762f693de93e60c
yalinigg25csc-droid <yalinigg25csc@vetias.ac.in>
2026-06-12 13:28:59 +0530

    first commit

--- src/Day7.java
+++ src/Day7.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+public static void main() {
+     class Request {
+        String name;
+        String date;
+        String reason;
 
-public class Dat7 {
+        Request(String name, String date, String reason) {
+            this.name = name;
+            this.date = date;
+            this.reason = reason;
+        }
+
+        void approve() {
+            System.out.println("Approving request");
+        }
+
+        void reject() {
+            System.out.println("Reject request");
+        }
+    }
+
+    static class LeaveRequest extends Request {
+
+        LeaveRequest(String name, String date, String reason) {
+            super(name, date, reason);
+        }
+
+        void approveLeaveRequest() {
+            System.out.println("Approving leave request");
+        }
+
+        void rejectLeaveRequest() {
+            System.out.println("Reject leave request");
+        }
+    }
+    static class OnDutyRequest extends Request {
+
+        OnDutyRequest(String name, String date, String reason) {
+            super(name, date, reason);
+        }
+
+        void approveOnDutyRequest() {
+            System.out.println("Approving on duty request");
+        }
+
+        void rejectOnDutyRequest() {
+            System.out.println("Reject on duty request");
+        }
+    }
+
+
+        LeaveRequest leaveRequest = new LeaveRequest("yalini", "12-06-2026", "health issues");
+        leaveRequest.rejectLeaveRequest();
+        OnDutyRequest onDutyRequest = new OnDutyRequest("yalini", "17-06-2026", "NCC camp");
+        onDutyRequest.approveOnDutyRequest();
+
 }
