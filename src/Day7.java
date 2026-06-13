847654ff25569a214b7114fc7d29d43f9fa2387b
yalinigg25csc-droid <yalinigg25csc@vetias.ac.in>
2026-06-13 11:19:05 +0530

    first commit

--- src/Day7.java
+++ src/Day7.java
@@ -1,4 +1,4 @@
-public class Day7 {
+/*public class Day7 {
      static class Payment {
         String amount;
 
@@ -21,6 +21,7 @@ public class Day7 {
             System.out.println("TRANSFER IS MADE");
         }
     }
+    }
 
     static class NBPayment extends Payment {
         @Override
@@ -38,4 +39,58 @@ public class Day7 {
         NBPayment nb = new NBPayment();
         nb.transfer();
     }
-}
+*/
+public class Day7 {
+
+    interface Employee {
+
+        String name = "xyz";
+        String id = "12345";
+        String salary = "50_000";
+
+
+        String name();
+    }
+
+    void solveproblem() {
+        System.out.println("solving the problem");
+
+    }
+
+    void work() {
+        System.out.println("working");
+    }
+
+    static class BankEmployee implements Employee {
+
+        @Override
+        public String name() {
+            System.out.println("name : " + name);
+            System.out.println("id : " + id);
+            System.out.println("salary : " + salary);
+
+            return "";
+        }
+    }
+        static class CheckingEmployee implements Employee {
+            @Override
+            public String name() {
+                System.out.println("name : " + name);
+                System.out.println("id : " + id);
+                System.out.println("salary : " + salary);
+                return "";
+
+            }
+        }
+
+            static void main() {
+                BankEmployee Bank = new BankEmployee();
+                Bank.name();
+
+                CheckingEmployee check = new CheckingEmployee();
+                check.name();
+
+            }
+    }
+
+
---
+++ src/Day8.java
@@ -0,0 +1,75 @@
+public  class Day8 {
+
+    interface Employee {
+        String Name();
+        String id();
+        String Salary();
+    }
+
+    static class BankEmployee implements Employee {
+
+        @Override
+        public String Name() {
+            return "shree";
+        }
+
+        @Override
+        public String id() {
+            return "105";
+        }
+
+        @Override
+        public String Salary() {
+            return "45000";
+        }
+
+        public String solveprogram() {
+            System.out.println("solution");
+            return "solution";
+        }
+    }
+
+    static class CheckingEmployee implements Employee {
+
+        @Override
+        public String Name() {
+            return "SHREE";
+        }
+
+        @Override
+        public String id() {
+            return "102";
+        }
+
+        @Override
+        public String Salary() {
+            return "45000";
+        }
+
+        public String work() {
+            System.out.println("process");
+            return "process";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        BankEmployee bank = new BankEmployee();
+
+        System.out.println("Name : " + bank.Name());
+        System.out.println("ID : " + bank.id());
+        System.out.println("Salary : " + bank.Salary());
+        bank.solveprogram();
+
+        System.out.println();
+
+        CheckingEmployee checking = new CheckingEmployee();
+
+        System.out.println("Name : " + checking.Name());
+        System.out.println("ID : " + checking.id());
+        System.out.println("Salary : " + checking.Salary());
+        checking.work();
+    }
+}
+
+
