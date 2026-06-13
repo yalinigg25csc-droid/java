03d2e9aaaf54ebda89ed4f694b6f9af8c9126e5b
yalinigg25csc-droid <yalinigg25csc@vetias.ac.in>
2026-06-13 12:27:36 +0530

    first commit

--- src/Day8.java
+++ src/Day8.java
@@ -1,87 +1,15 @@
- public  class Day8 {
 
-   interface Employee {
-        String Name();
-        String id();
-        String Salary();
-    }
+     public class Day8 {
+         record Coursevetrecord(String course_name, String dept) {
 
-    static class BankEmployee implements Employee {
+         }
 
-        @Override
-        public String Name() {
-            return "shree";
-        }
+         static void main() {
 
-        @Override
-        public String id() {
-            return "105";
-        }
-
-        @Override
-        public String Salary() {
-            return "45000";
-        }
-
-        public String solveprogram() {
-            System.out.println("solution");
-            return "solution";
-        }
-    }
-
-    static class CheckingEmployee implements Employee {
-
-        @Override
-        public String Name() {
-            return "SHREE";
-        }
-
-        @Override
-        public String id() {
-            return "102";
-        }
-
-        @Override
-        public String Salary() {
-            return "45000";
-        }
-
-        public String work() {
-            System.out.println("process");
-            return "process";
-        }
-    }
-
-    public static void main(String[] args) {
-
-        BankEmployee bank = new BankEmployee();
-
-        System.out.println("Name : " + bank.Name());
-        System.out.println("ID : " + bank.id());
-        System.out.println("Salary : " + bank.Salary());
-        bank.solveprogram();
-
-        System.out.println();
-
-        CheckingEmployee checking = new CheckingEmployee();
-
-        System.out.println("Name : " + checking.Name());
-        System.out.println("ID : " + checking.id());
-        System.out.println("Salary : " + checking.Salary());
-        checking.work();
-    }
-}
-
-
-     record Coursevetrecord(String course_name, String dept) {
-
+             Coursevetrecord coursevet = new Coursevetrecord("PYTHON", "CS");
+             System.out.println(coursevet.course_name);
+             System.out.println(coursevet.dept);
+         }
      }
-
-     static void main() {
-         Coursevetrecord coursevet = new Coursevetrecord("PYTHON","CS");
-         System.out.println(coursevet.course_name);
-         System.out.println(coursevet.dept);
-     }
- }
 
 
