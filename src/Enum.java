d5cf77cd0feae1fe241c3ba2dc79dafe52b3122a
yalinigg25csc-droid <yalinigg25csc@vetias.ac.in>
2026-06-13 14:12:22 +0530

    first commit

---
+++ src/Enum.java
@@ -0,0 +1,19 @@
+public class Enum {
+    enum LANGUAGE{
+        EN("English"),
+     GR("German"),
+     JP("Japanese"),
+     TA("Tamil");
+String description ;
+LANGUAGE (String description){
+    this.description = description;
+}
+    }
+
+    static void main() {
+        System.out.println(LANGUAGE.TA.description);
+        System.out.println(LANGUAGE.GR.description);
+        System.out.println(LANGUAGE.EN.description);
+        System.out.println(LANGUAGE.JP.description);
+    }
+}
