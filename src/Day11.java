841d2771b06783e0d1f9aa6535445b4d11862aec
yalini <yalinigg25csc@vetias.ac.in>
2026-06-17 14:12:12 +0530

    adding set

---
+++ src/Day11.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day11 {
+    public static void main(String[]args) {
+        Set<String> set = new HashSet<>();
+set.add("maya");
+set.add("maya");
+set.add("yalini");
+set.add("yalu");
+set.add("yalini");
+System.out.println("set : ");
+for (String s: set){
+    System.out.println(s);
+}
+        }
+
+    }
+
