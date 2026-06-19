172a29d21b3b4c8572f28f09f5f6ec0c90f5b611
yalini <yalinigg25csc@vetias.ac.in>
2026-06-19 15:59:03 +0530

    csv

---
+++ src/Day13.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day13 {
+
+
+    /*static void main() {
+        Path path = Path.of("files", "report.txt");
+        try {
+            List<String> data = Files.readAllLines(path);
+
+            for (String line : data) {
+                System.out.println(line);
+            }
+            List<String> newData = new ArrayList<>();
+            newData.add("orange");
+            newData.add("mango");
+            newData.add("grapes");
+            newData.add("apple");
+            newData.add("jamun");
+
+            Files.write(path,newData);
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+}
+
+*/
+
+    static void main() {
+        Path path = Path.of("files", "students.csv");
+        try {
+            List<String> students = Files.readAllLines(path);
+
+            for (String line : students) {
+                String[] row = line.split(",");
+                for (int i = 0; i < row.length; i++) {
+                    System.out.print(row[i] + " | ");
+                }
+                System.out.println();
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+}
+
